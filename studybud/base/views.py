7d11bc829fd31ce60db9cc403fe0ea2ae84c86a6
codewheezy <uchennaibe95@gmail.com>
2022-01-28 16:15:40 +0100

    created form model

--- studybud/base/views.py
+++ studybud/base/views.py
@@ -1,5 +1,6 @@
 from django.shortcuts import render
 from .models import Room
+from .forms import RoomForm
 
 # Create your views here.
 
@@ -18,4 +19,13 @@ def room(request, pk):
     room = Room.objects.get(id=pk)
     context = {'room': room}
     return render(request, 'base/room.html', context)
+
+def createRoom(request):
+    form = RoomForm()
+    if request.method == 'POST':
+        form = RoomForm(request.POST)
+        if form.is_valid():
+            form.save()
+    context = {'form': form}
+    return render(request, 'base/room_form.html', context)
 
